d911f491a1aece680459c63962a893c737baf1c1
Jer Carlo <jercarlopogi07@gmail.com>
2020-07-16 09:01:48 +0800

    all results font change and space and indentation fix

--- src/components/AllResults.js
+++ src/components/AllResults.js
@@ -7,12 +7,11 @@ class AllResults extends Component {
             return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
           }
         return (
-            <div className="results">
-         <div className="results__item">Total Cases: <span className="results__data">{formatNumber(this.props.result.cases)}</span></div>
-         <div className="results__item ">Total Deaths: <span className="results__data results__data--bad">{formatNumber(this.props.result.deaths)}</span></div>
-            <div className="results__item">Total Active: <span className="results__data">{formatNumber(this.props.result.active)}</span></div>
-            <div className="results__item ">Total Recovered: <span className="results__data results__data--good">{formatNumber(this.props.result.recovered)}</span></div>
-        
+        <div className="results">
+                <div className="results__item">Total Cases: <span className="results__data">{formatNumber(this.props.result.cases)}</span></div>
+                <div className="results__item ">Total Deaths: <span className="results__data results__data--bad">{formatNumber(this.props.result.deaths)}</span></div>
+                <div className="results__item">Total Active: <span className="results__data">{formatNumber(this.props.result.active)}</span></div>
+                <div className="results__item ">Total Recovered: <span className="results__data results__data--good">{formatNumber(this.props.result.recovered)}</span></div>
         </div>
         )
     }
